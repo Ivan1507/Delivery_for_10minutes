9f9ddb5c8255cfd1013efaca7b3f0972f2103499
Alexander <lucky-pa777@yandex.ru>
2020-11-18 17:18:29 +0300

    fix

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -26,16 +26,16 @@ public class Main extends Application {
         deliveryLogic.DeliveryData=FXCollections.observableArrayList();
 
 
-        BaseTransport Car = new BaseTransport(146+80,147+7);
+        BaseTransport Car = new Quadrocopter(146+80,147+7);
         Car.setName("Машина 2");
         Car.setPointType(PointType.Circle);
         deliveryLogic.getDepartment().getVehicles().add( Car );
 
 
-//        BaseTransport Car3 = new Quadrocopter(46+80,147+7);
-//        Car3.setMaxSpeed(1);
-//        Car3.setPointType(PointType.Circle);
-//        deliveryLogic.getDepartment().getVehicles().add( Car3 );
+        BaseTransport Car3 = new Quadrocopter(46+80,147+7);
+        Car3.setMaxSpeed(1);
+        Car3.setPointType(PointType.Circle);
+        deliveryLogic.getDepartment().getVehicles().add( Car3 );
 
 
 //        BaseTransport Car3 = new Quadrocopter(206+80,157+25);
