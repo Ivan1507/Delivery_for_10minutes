d75c3c7aa43d0396069e1e6aeb08bd65173acc9d
Ivan1507 <iancurtis1507@mail.ru>
2020-12-16 08:21:59 +0300

    Done working with histogramm(100%)

--- src/sample/Controllers/ChartController.java
+++ src/sample/Controllers/ChartController.java
@@ -14,12 +14,11 @@ import sample.Delivery.DeliveryStatus;
 
 import java.io.*;
 import java.net.URL;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ResourceBundle;
-import java.util.TreeMap;
+import java.util.*;
 
 public class ChartController implements Initializable {
     private  XYChart.Series set=new XYChart.Series();
@@ -31,61 +30,156 @@ public class ChartController implements Initializable {
     @FXML
     private DatePicker datePicker;
     @FXML
+    private DatePicker datePicker1;
+    @FXML
     private void click() throws IOException, ClassNotFoundException {
+        boolean f=false;
         clear();
-        chart.setTitle("Статистика заказов за "+datePicker.getValue());
         File  file = new File("./date.txt");
         FileInputStream fileInputStream = new FileInputStream(file);
         ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
         HashMap<String, Map<String, Integer>> map =(HashMap<String,Map<String,Integer>>) objectInputStream.readObject();
-        System.out.println("datePicker = " + datePicker.getValue().toString());
-        //Получить инфу о доставках
-        if(map.get(datePicker.getValue().toString())!=null) {
-            Integer a=map.get(datePicker.getValue().toString()).get("<");
-            XYChart.Data data = new XYChart.Data("до 5", map.get(datePicker.getValue().toString()).get("<"));
-            set2.getData().add(data);
-            Integer b=map.get(datePicker.getValue().toString()).get("=");
-            XYChart.Data data1 = new XYChart.Data("от 5 до 10 мин", map.get(datePicker.getValue().toString()).get("="));
-            set1.getData().add(data1);
-            Integer c=map.get(datePicker.getValue().toString()).get(">");
-            XYChart.Data data2 = new XYChart.Data("от 10 мин", map.get(datePicker.getValue().toString()).get(">"));
-            set.getData().add(data2);
-            chart.getData().addAll(set2, set1, set);
-            Node nl = chart.lookup(".default-color0.chart-bar");
-            Node ns = chart.lookup(".default-color1.chart-bar");
-            Node nsl = chart.lookup(".default-color2.chart-bar");
-            nl.setStyle("-fx-bar-fill:green");
-            ns.setStyle("-fx-bar-fill:orange;");
-            nsl.setStyle("-fx-bar-fill:red;");
-            //Получить рекомендации
-            if(c>=(a+b)*0.5){
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Инфорация от администратора");
-                alert.setHeaderText("Необходимо добавить количество транспортных связей!");
-                alert.show();
-            }
-            else if(a>=(b+c)*0.5){
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Инфорация от администратора");
-                alert.setHeaderText("Можно уменшить количество транспортных связей!");
-                alert.show();
-            }
-            else if(b>=(c+a)*0.5){
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Инфорация от администратора");
-                alert.setHeaderText("Ваша Доставка нормально справляется с заказами!");
-                alert.show();
-            }
+        boolean go=false;
+        int ld = 0,ly=0,lm=0,rd = 0,ry = 0,rm=0;
 
-        }
-        else {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Информация от администратора");
-            alert.setHeaderText("В этот день не было доставок!");
-            alert.show();
-        }
+        if(datePicker.getValue()!=null&&datePicker1.getValue()!=null) {
+            go=true;
+            ld = datePicker.getValue().getDayOfMonth();
+            ly = datePicker.getValue().getYear();
+            lm = datePicker.getValue().getMonthValue();
 
+            rd = datePicker1.getValue().getDayOfMonth();
+            ry = datePicker1.getValue().getYear();
+            rm = datePicker1.getValue().getMonthValue();
+            if (ly > ry) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Инфорация от администратора");
+                alert.setHeaderText("Введены некорректные данные");
+                alert.show();
+                chart.setTitle("Введены некорректные данные");
+                f = true;
+            } else if (lm > rm&&ry==ly) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Инфорация от администратора");
+                alert.setHeaderText("Введены некорректные данные");
+                alert.show();
+                chart.setTitle("Введены некорректные данные");
+                f = true;
+            } else if (ld > rd && lm==rm) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Инфорация от администратора");
+                alert.setHeaderText("Введены некорректные данные");
+                alert.show();
+                chart.setTitle("Введены некорректные данные");
+                f = true;
+            }
+        }
+        if(!f&&go) {
+            chart.setTitle("Статистика заказов с "+datePicker.getValue()+" по "+datePicker1.getValue());
+            //Получить инфу о доставках
+            if (map.get(datePicker.getValue().toString()) != null) {
+                Integer a=0;
+                Integer b=0;
+                Integer c=0;
+                a=map.get(datePicker.getValue().toString()).get("<");
+                b=map.get(datePicker.getValue().toString()).get("=");
+                c=map.get(datePicker.getValue().toString()).get(">");
+                System.out.println("values" + a+"/"+b+"/"+c);
+
+                ZoneId defaultZoneId = ZoneId.systemDefault();
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(Date.from(datePicker.getValue().atStartOfDay(defaultZoneId).toInstant()));
+                while(ld<rd||lm<rm||ly<ry) {
+                    cal.add(Calendar.DATE,1);
+                    LocalDate next = LocalDate.ofInstant(cal.toInstant(), defaultZoneId);
+                    System.out.println("next = " + next);
+                    ld=next.getDayOfMonth();
+                    lm=next.getMonthValue();
+                    ly=next.getYear();
+                    System.out.println("ld = " + ld);
+                    System.out.println("lm = " + lm);
+                    System.out.println("ly = " + ly);
+                    if(map.get(next.toString())!=null) {
+                        if (map.get(next.toString()).get("<") != null)
+                            a += map.get(next.toString()).get("<");
+                        if (map.get(next.toString()).get("=") != null)
+                            b += map.get(next.toString()).get("=");
+                        if (map.get(next.toString()).get(">") != null)
+                            c += map.get(next.toString()).get(">");
+                    }
+                    System.out.println("values" + a+"/"+b+"/"+c);
+                }
+                
+                
+                XYChart.Data data = new XYChart.Data("до 5", a);
+                if (a != null)
+                    set2.getData().add(data);
+                
+                XYChart.Data data1 = new XYChart.Data("от 5 до 10 мин", b);
+                if (b != null)
+                    set1.getData().add(data1);
+                XYChart.Data data2 = new XYChart.Data("от 10 мин", c);
+                if (c != null)
+                    set.getData().add(data2);
+
+                if (a != null) {
+                    chart.getData().add(set2);
+                    Node nl = chart.lookup(".default-color0.chart-bar");
+                    nl.setStyle("-fx-bar-fill:green");
+                }
+                if (b != null) {
+                    chart.getData().add(set1);
+                    Node ns = chart.lookup(".default-color1.chart-bar");
+                    ns.setStyle("-fx-bar-fill:orange;");
+                }
+                if (c != null) {
+                    chart.getData().add(set);
+                    Node nsl = chart.lookup(".default-color2.chart-bar");
+                    nsl.setStyle("-fx-bar-fill:red;");
+                }
+
+                //Получить рекомендации
+                if (a != null && b != null && c != null) {
+                    if (c >= (a + b) * 0.5) {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Инфорация от администратора");
+                        alert.setHeaderText("Необходимо добавить количество транспортных связей!");
+                        alert.show();
+                    } else if (a >= (b + c) * 0.5) {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Инфорация от администратора");
+                        alert.setHeaderText("Можно уменшить количество транспортных связей!");
+                        alert.show();
+                    } else if (b >= (c + a) * 0.5) {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Инфорация от администратора");
+                        alert.setHeaderText("Ваша Доставка нормально справляется с заказами!");
+                        alert.show();
+                    }
+                } else if (a == null) {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Инфорация от администратора");
+                    alert.setHeaderText("Необходимо добавить количество транспортных связей!");
+                    alert.show();
+                } else if (c == null) {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Инфорация от администратора");
+                    alert.setHeaderText("Можно уменшить количество транспортных связей!");
+                    alert.show();
+                }
+
+
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Информация от администратора");
+                alert.setHeaderText("За этот период не было доставок!");
+                alert.show();
+            }
+        }
     }
+
+
+
     @FXML
     private void clear(){
         set.getData().clear();
@@ -103,11 +197,6 @@ public class ChartController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        //chart.setTitle("Статистика заказов");
-//        addData(4);
-//        addData(7);
-//        addData(8);
-//        addData(11);
         chart.setLegendVisible(false);
         //chart.getData().addAll(set,set1,set2);
 
